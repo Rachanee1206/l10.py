1ca1efe64cf6b11971eb496cedd88063609b0fe4
Rachanee1206 <rachaneeb.t.r1206@gmail.com>
2026-01-06 20:05:11 +0700

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,9 @@
+def wishes(n):
+    print("Hello", n, "How are you?")
+    print("welcome to class")
+    print("Have a nice day")
+
+wishes("Yashu")
+wishes("Radha")
+wishes("Gawgu")
+wishes("Paglu")
---
+++ a2.py
@@ -0,0 +1,7 @@
+def weather(w,c):
+    print("Today's weater is",w)
+    print ("Its too",c,"\n")
+
+weather("sunny","hot")
+weather("windy","cold")
+weather("rainy","cool")
